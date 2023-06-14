0b6ed55f814136a146975ce194c3691c1d7af4ed
Huynh Chau <s3777214@rmit.edu.vn>
2023-06-14 14:47:07 +0700

    update the two tabs in left nav bar

--- app.py
+++ app.py
@@ -4,33 +4,11 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc 
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-import functions.file_operations
+from functions import file_operations
+from functions.upload import upload_component
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-upload_component = dcc.Upload(
-    id="upload-data",
-    children=[
-        html.Div([
-            html.Img(src="https://via.placeholder.com/150", id="upload-preview"),
-            html.P("Drag and drop or click to select a file to upload."),
-        ], id="upload-area"),
-    ],
-    style={
-        "width": "100%",
-        "height": "200px",
-        "borderWidth": "2px",
-        "borderStyle": "dashed",
-        "borderRadius": "5px",
-        "textAlign": "center",
-        "margin": "10px",
-        "display": "flex",
-        "flexDirection": "column",
-        "justifyContent": "center",
-        "alignItems": "center",
-    },
-    multiple=True,  # Allow multiple file uploads
-)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
 download_button = html.Div(
     dbc.Button(
@@ -51,50 +29,134 @@ left_navbar = html.Div(
         'backgroundColor': 'black',
         'color': 'white',
         'padding': '20px',
-        'height': '100vh',
         'display': 'flex',
-        'flexDirection': 'row',
+        'flexDirection': 'column',
+        'alignItems': 'center',
         'justifyContent': 'flex-start',
+        'height': '100vh',
     },
     children=[
         html.Div(
             style={
                 'display': 'flex',
                 'flexDirection': 'column',
+                'alignItems': 'center',
             },
             children=[
                 html.Img(
-                            src='/assets/rmitLogo.jpg',
-                            style={'width': '200px', 'height': 'auto', 'margin-top': '10px', 'marginLeft': 'auto'}
-                        ),
+                    src='/assets/rmitLogo.jpg',
+                    style={'width': '200px', 'height': 'auto', 'margin-top': '10px'}
+                ),
                 html.H2('Engineering Capstone project', style={'color': 'white'}),
                 html.H3('Group name: Helios Negotiator'),
                 html.Br(),
+            ]
+        ),
+        
+        # Add tabs to the left navbar
+        dbc.Tabs(
+            [
+                dbc.Tab(label="Project Description", tab_id="tab-1", labelClassName="nav-link", activeLabelClassName="active"),
+                dbc.Tab(label="Project Team", tab_id="tab-2", labelClassName="nav-link", activeLabelClassName="active"),
+            ],
+            id="tabs",
+            className="nav nav-pills mt-4",
+            active_tab="tab-1",
+            style={'display': 'flex', 'flexDirection': 'row'},
+        ),
+        
+        # Content for each tab
+        html.Div(id="tab-content"),
+    ]
+)
+
+tab_1 = html.Div(
+    children=[
+        html.Div(
+            [
+                html.P(
+                    "This project proposal aims to detect and reduce losses in solar panels caused by damaged cells. "
+                    "By utilizing UAV-based thermal imaging processing, we can quickly spot and identify faulty cells, "
+                    "streamlining the maintenance process. Through data augmentation and machine learning algorithms, "
+                    "subsequent images can be automatically analyzed for faults, significantly reducing downtime "
+                    "and increasing overall efficiency for solar power generation.",
+                    style={'text-align': 'justify'}
+                )
+            ],
+            style={
+                'display': 'flex',
+                'flex-direction': 'column',
+                'justify-content': 'center',
+                'align-items': 'center',
+                'height': '400px',
+                'width': '300px',
+                'margin': 'auto',
+            },
+        )
+    ],
+    style={'height': '100vh'},
+)
+
+tab_2 = html.Div(
+    children=[
+        html.Div(
+            [
                 html.H5('Student name: Nguyen Dang Huynh Chau (s3777214)'),
                 html.H5('Student name: To Vu Phuc (s3758272)'),
                 html.H5('Student name: Nguyen Nhat Tan (s3818559)'),
                 html.H5('Student name: Tong Son Tung (s3818153)'),
-            ]
-        ),
-        
-    ]
+            ],
+            style={
+                'display': 'flex',
+                'flex-direction': 'column',
+                'justify-content': 'center',
+                'align-items': 'center',
+                'height': '300px',
+                'width': '300px',
+                'margin': 'auto',
+            },
+        )
+    ],
+    style={'height': '100vh'},
 )
+
+
+
+# Callback to update the content based on the selected tab
+@app.callback(
+    Output("tab-content", "children"),
+    Input("tabs", "active_tab"),
+)
+def render_tab_content(active_tab):
+    if active_tab == "tab-1":
+        return tab_1
+    elif active_tab == "tab-2":
+        return tab_2
+    else:
+        return html.P("No content available for this tab.")
+
+
 
 app.layout = html.Div(
     style={'display': 'flex'},
     children=[
         left_navbar,
-        html.Div(className='content', style={'flex': '1', 'backgroundColor': 'white', 'padding': '20px'}, children=[
-            html.H2("Upload your image:", style={'textAlign': 'center'}),
-            upload_component,
-            html.H2("File List"),
-            html.Ul(id="file-list"),
-            download_button,
-            dcc.Location(id="url", refresh=False),
-            dcc.Download(id="download"),
-        ])
+        html.Div(
+            className='content',
+            style={'flex': '1', 'backgroundColor': 'white', 'padding': '20px'},
+            children=[
+                html.H2("Upload your image:", style={'textAlign': 'center'}),
+                upload_component,  # Use the upload_component from functions.upload
+                html.H2("File List"),
+                html.Ul(id="file-list"),
+                download_button,
+                dcc.Location(id="url", refresh=False),
+                dcc.Download(id="download"),
+            ]
+        )
     ]
 )
+
 
 
 @app.callback(
@@ -122,6 +184,19 @@ def download_files(n_clicks, file_list_items):
 
     filenames = [item["props"]["children"] for item in file_list_items]
     return file_operations.download_files(filenames)
+
+# Add callback for tab selection
+@app.callback(
+    Output("tab-1", "active"),
+    Output("tab-2", "active"),
+    Input("url", "pathname")
+)
+def toggle_tab(pathname):
+    if pathname == "/tab-1":
+        return True, False
+    elif pathname == "/tab-2":
+        return False, True
+    return False, False
 
 
 if __name__ == "__main__":
--- functions/upload.py
+++ functions/upload.py
@@ -1,6 +1,33 @@
 import base64
+import dash_html_components as html
+import dash_core_components as dcc
 
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
+upload_component = dcc.Upload(
+    id="upload-data",
+    children=[
+        html.Div([
+            html.Img(src="https://via.placeholder.com/150", id="upload-preview"),
+            html.P("Drag and drop or click to select a file to upload."),
+        ], id="upload-area"),
+    ],
+    style={
+        "width": "100%",
+        "height": "200px",
+        "borderWidth": "2px",
+        "borderStyle": "dashed",
+        "borderRadius": "5px",
+        "textAlign": "center",
+        "margin": "10px",
+        "display": "flex",
+        "flexDirection": "column",
+        "justifyContent": "center",
+        "alignItems": "center",
+    },
+    multiple=True,  # Allow multiple file uploads
+)
+
 
 def save_file(name, content):
     """Decode and save the file content."""
